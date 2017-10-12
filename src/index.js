1757f3d7de7c6e62a3ebcbfe84a0b99ba12a5039
Rafael Nunes G. da Silveira <rafaelngds@gmail.com>
2017-10-12 13:23:43 -0300

    feat(index): add index file and app entry point

--- src/index.js
+++ src/index.js
@@ -1,49 +1,5 @@
-function renderParallelSet(selector, data) {
-  const chart = d3.parsets();  
-  const chartContainer = d3.select(selector);
-  console.log(chartContainer)
-  const width = chartContainer.node().getBoundingClientRect().width;
-
-  chartContainer.select("svg").remove();
-
-  var svg = chartContainer.append("svg")
-    .attr("style", "background-color: #f5f5f5")
-    .attr("width", width)
-    .attr("height", chart.height());
-
-  var filteredCategories = getFilteredCategories();
-
-  chart.dimensions(filteredCategories);
-  chart.width(width);
-
-  const chartData = data.reduce((list, element) => {
-    list.push({
-      Status: element.status,
-      Category: element.category,
-    });
-
-    return list;
-  }, []);
-
-  svg.datum(chartData).call(chart);
-}
-
-var data = [
-  {
-    status: 200,
-    category: "cosmeticos"
-  },
-  {
-    status: 404,
-    category: "cosmeticos"
-  }
-]
-
-const setupParsetFunction = require('./d3.parsets.js');
-setupParsetFunction(d3);
-
-window.onload = function() {
-  console.log('window - onload'); // 4th
-
-  renderParallelSet(".content", data);
-};
+document.addEventListener('DOMContentLoaded', () => {
+  readData(() => {
+    console.log("loaded")
+  })
+})
