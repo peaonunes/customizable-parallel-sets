0eead4d002b942a851915d2a9610dbdbb68a41d4
Rafael Nunes G. da Silveira <rafaelngds@gmail.com>
2017-10-12 13:34:53 -0300

    feat(index): add sidebar checkbox based on data loaded

--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
+import './assets/app.scss'
+const d3 = require('d3')
+
 document.addEventListener('DOMContentLoaded', () => {
-  readData(() => {
-    console.log("loaded")
+  readData((data) => {
+    const fields = getDataFields(data)
+    renderSideBar(fields)
   })
 })
+
+function readData(callback) {
+  const data = [
+    {
+      status: 200,
+      gender: "Male"
+    },
+    {
+      status: 422,
+      gender: "Female"
+    }
+  ]
+
+  if(callback) {
+    callback(data)
+  }
+}
+
+function getDataFields(data) {
+  return Object.keys(data[0])
+}
+
+function renderSideBar(fields) {
+  const sidebar = d3.select(".sidebar");
+  sidebar
+    .selectAll("input")
+    .data(fields)
+    .enter()
+    .append("label")
+      .attr("for", (d) => d)
+      .text((d) => d)
+    .append("input")
+      .attr("checked", true)
+      .attr("type", "checkbox")
+      .attr("id", (d) => d)
+      .on("click", (d) => console.log(`Pressed ${d}`))
+}
