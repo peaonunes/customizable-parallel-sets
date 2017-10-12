367ac3a4d1ad66dcdcbeb9319e79b56f863cee43
Rafael Nunes G. da Silveira <rafaelngds@gmail.com>
2017-10-12 13:22:50 -0300

    feat(webpack.config.js): configure webpack with package json dependencies

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: './src/index.js',
+
+  resolve: {
+    extensions: [ '', '.js', '.jsx' ],
+  },
+
+  devtool: 'source-map',
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'source-map',
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: { presets: [ 'es2015' ] },
+      },
+      {
+        test: /.scss$/,
+        loaders: [ 'style', 'css', 'postcss', 'sass' ],
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      d3: 'd3',
+    }),
+  ],
+
+  externals:[{
+    xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}'
+  }],
+
+  postcss () {
+    return [autoprefixer];
+  },
+};
